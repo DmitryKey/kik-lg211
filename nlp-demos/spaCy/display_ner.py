20857e3f96a5c6f480a5e220ac1d564d71be87ab
dmitry.kan <dmitry.kan@alpha-sense.com>
2018-11-21 11:17:50 +0200

    #2 spaCy NER demos: updated during the demos

--- nlp-demos/spaCy/display_ner.py
+++ nlp-demos/spaCy/display_ner.py
@@ -1,7 +1,10 @@
 import spacy
 from spacy import displacy
 
-text = u"""In 2005, "The Guardian" determined using "estimates of earnings accrued in a composer's lifetime" that George Gershwin was the wealthiest composer of all time. In September 2013, a partnership between the estates of Ira and George Gershwin and the University of Michigan was created and will provide the university's School of Music, Theatre, and Dance access to Gershwin's entire body of work, which includes all of Gershwin's papers, compositional drafts, and scores. This direct access to all of his works will provide opportunities to musicians, composers, and scholars to analyze and reinterpret his work with the goal of accurately reflecting the composers' vision in order to preserve his legacy. The first fascicles of "The Gershwin Critical Edition", edited by Mark Clague, are expected in 2017; they will cover the 1924 jazz band version of "Rhapsody in Blue", "An American in Paris" and "Porgy and Bess"."""
+text = u"""With a degree of frustration, George tried various piano teachers for some two years (circa. 1911) before finally being introduced to Charles Hambitzer by Jack Miller (circa. 1913), the pianist in the Beethoven Symphony Orchestra. Until his death in 1918, Hambitzer remained Gershwin's musical mentor and taught him conventional piano technique, introduced him to music of the European classical tradition, and encouraged him to attend orchestral concerts. Following such concerts, young Gershwin would essentially try to play, on the piano at home, the music he had heard from recall, and without sheet music. As a matter of course, Gershwin later studied with the classical composer Rubin Goldmark and avant-garde composer-theorist Henry Cowell, thus formalizing his classical music training.
+
+In 1913, Gershwin left school at the age of 15 and found his first job as a "song plugger". His employer was Jerome H. Remick and Company, a Detroit-based publishing firm with a branch office on New York City's Tin Pan Alley, and he earned $15 a week.
+"""
 
 nlp = spacy.load('en_core_web_sm')
 doc = nlp(text)
